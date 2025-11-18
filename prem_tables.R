f0bb660d205de450dd9616d8c867371e62aaf490
andrewmoles2 <andrewmoles2@gmail.com>
2025-11-18 15:00:46 +0000

    add df for each season

--- prem_tables.R
+++ prem_tables.R
@@ -1,26 +1,26 @@
 # premier league table historical
 # scraped from wikipedia
 
-# libraries
+# libraries ----
 library(tidyverse)
 library(rvest)
 library(polite)
 library(glue)
 library(janitor)
 
-# setup years and seasons
+# setup years and seasons ----
 years <- seq(1992, 2024, 1)
 years_sub <- substr(years + 1, 3, 4)
 
 season <- glue("{years}-{years + 1}")
 
-# vectors for setting up links
+# vectors for setting up links ----
 fa_1 <- 1:7
 nfa_1 <- 8
 fa_2 <- 9:15
 nfa_2 <- 16:length(years)
 
-# making the links
+# making the links ----
 url_list <- c(
   glue("https://en.wikipedia.org/wiki/{years[fa_1]}%E2%80%93{years_sub[fa_1]}_FA_Premier_League"),
   glue("https://en.wikipedia.org/wiki/{years[nfa_1]}%E2%80%93{years_sub[nfa_1]}_Premier_League"),
@@ -28,7 +28,7 @@ url_list <- c(
   glue("https://en.wikipedia.org/wiki/{years[nfa_2]}%E2%80%93{years_sub[nfa_2]}_Premier_League")
 )
 
-# extracting just the league tables for each season
+# extracting just the league tables for each season ----
 prem_table_list <- list()
 
 for (i in seq_along(url_list)) {
@@ -46,7 +46,7 @@ for (i in seq_along(url_list)) {
   Sys.sleep(1)
 }
 
-# adding the season to the data frames
+# adding the season to the data frames ----
 prem_table_list_tidy <- c()
 
 for (i in seq_along(prem_table_list)) {
@@ -57,31 +57,47 @@ for (i in seq_along(prem_table_list)) {
 
 names(prem_table_list_tidy) <- season
 
-# fix 19-20 col names
+# fix 19-20 col names 
 table_cols <- colnames(prem_table_list_tidy[["2018-2019"]])
 colnames(prem_table_list_tidy[["2019-2020"]]) <- table_cols
 
-# combine to one data frame for ease of use and saving
+# tidy each data frame in list ----
+
+for (s in seq_along(prem_table_list_tidy)) {
+  prem_table_list_tidy[[s]] <- prem_table_list_tidy[[s]] |>
+    clean_names() |>
+    mutate(
+      team = str_replace(team, "\\(.*", ""),
+      qualification_or_relegation = str_replace(qualification_or_relegation,
+                                                "\\[.*", ""),
+      pts = str_replace(pts, "\\[.*", ""),
+      pts = as.integer(pts)
+    ) |>
+    mutate(
+      gd = gf - ga
+    )
+}
+
+# combine to one data frame for ease of use and saving ----
 all_prem_years <- data.table::rbindlist(prem_table_list_tidy)
+# other options
 #all_prem_years <- bind_rows(prem_table_list_tidy)
 #all_prem_years <- do.call("rbind", prem_table_list_tidy)
 
-# tidy up the data frame
-all_prem_years_clean <- all_prem_years %>%
-  clean_names() %>%
-  mutate(
-    team = str_replace(team, "\\(.*", ""),
-    qualification_or_relegation = str_replace(qualification_or_relegation,
-                                              "\\[.*", ""),
-    pts = str_replace(pts, "\\[.*", ""),
-    pts = as.integer(pts)
-  ) %>%
-  mutate(
-    gd = gf - ga
-  ) 
-
+# save combined dataset ----
 write_csv(all_prem_years_clean, "data/premier_league_tables.csv")
 
-# optional save extra
-# make a folder in data and save each year there
+# save data for each season ----
+if (dir.exists("data/seasons") == FALSE) {
+  print("No seasons directory, making it now....")
+  dir.create("data/seasons")
+} else {
+  print("seaons directory already exists!")
+}
+
+for (save_file in names(prem_table_list_tidy)) {
+  write_csv(prem_table_list_tidy[[save_file]],
+            file = paste0("data/seasons/", save_file, ".csv"))
+}
+
 
